09e00b6913a6f26a61404829ecd12d66ddbc25ed
TheMshary <m.alsharekh94@gmail.com>
2020-10-20 14:20:01 +0300

    Cookie List

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// Data
+import cookies from "./cookies";
+
+// Styles
+import styles from "./styles";
 
 function App() {
+  const cookieList = cookies.map(cookie => <div style={styles.cookie} key={cookie.id}>
+    <img style={styles.cookieImage} src={cookie.image} alt="cute cookie" />
+    <p style={styles.text}>{cookie.name}</p>
+    <p style={styles.text}>{cookie.price} KD</p>
+  </div>)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1 style={styles.text}>Cookies and Beyond</h1>
+        <h4 style={styles.text}>Where cookie maniax gather</h4>
+        <img style={styles.shopImage} src="https://i.pinimg.com/originals/8f/cf/71/8fcf719bce331fe39d7e31ebf07349f3.jpg" alt="cookie shop" />
+      </div>
+      <div style={styles.list}>
+        {cookieList}
+      </div>
     </div>
   );
 }
---
+++ src/styles.js
@@ -0,0 +1,20 @@
+const styles = {
+  text: { textAlign: "center" },
+  shopImage: {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "50%"
+  },
+  cookieImage: {
+    width: "200px", height: "200px"
+  },
+  list: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  cookie: { margin: "20px" }
+}
+
+export default styles;
